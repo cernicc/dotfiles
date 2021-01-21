f12dd4460cee4648f7804373a0bcf97a5a17902f
Rok Černič <rok.cernic@gmail.com>
2021-01-21 22:04:24 +0100

    handle container mode

--- install.sh
+++ install.sh
@@ -2,11 +2,6 @@
 
 set -e # -e: exit on error
 
-if [[ $* == *--container* ]]; then
-  echo "Running in container mode."
-  export CODESPACES=true
-fi
-
 if [ ! "$(command -v chezmoi)" ]; then
   bin_dir="$HOME/.local/bin"
   chezmoi="$bin_dir/chezmoi"
@@ -25,4 +20,8 @@ fi
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 # exec: replace current process with chezmoi init
-exec "$chezmoi" init --apply "--source=$script_dir"
+if [[ $* == *--container* ]]; then
+  exec env CODESPACES=true "$chezmoi" init --apply "--source=$script_dir"
+else
+  exec "$chezmoi" init --apply "--source=$script_dir"
+fi
